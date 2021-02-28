8bf3f53adf2eae120d24d25ef08c1a64149121b1
Jon Barker <j.p.barker@sheffield.ac.uk>
2021-02-28 11:24:25 +0000

    added openMHA bin and lib paths to the path.sh (built from: 9a0d562)

--- projects/MSBG_python/packages/matlab_mldivide/matlab_mldivide.m
+++
@@ -1,8 +0,0 @@
-function [ C ] = matlab_mldivide( A, B )
-%MATLAB_MLDIVIDE Summary of this function goes here
-%   Detailed explanation goes here
-
-C = A \ B;
-
-end
-
--- scripts/paths.sh
+++ scripts/paths.sh
@@ -3,3 +3,5 @@ export CLARITY_ROOT=$DIRECTORY/..
 export CLARITY_DATA="$CLARITY_ROOT"/data/clarity_data
 export PYTHON_BIN="$CLARITY_ROOT"/env/bin/python
 
+export LD_LIBRARY_PATH="$CLARITY_ROOT"/tools/openMHA/lib:$LD_LIBRARY_PATH
+export PATH="$CLARITY_ROOT"/tools/openMHA/bin:$PATH
